7a1fd20402a5b20582ec86126bc3a6c3fc1eec5e
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-01-27 15:39:53 -0800

    About page changes

--- falling-stars/src/About/About.js
+++ falling-stars/src/About/About.js
@@ -6,17 +6,34 @@ class About extends PureComponent {
     return (
       <div className="About column-container">
         <div className="row-container">
-          <div className="blurred-text">quote</div>
-          <div>
+          <div className="column-container">
+            <div className="quote column-container">
+              <div className="quote-text blurred-text">
+                "For my part I know nothing with any certainty, but the sight of
+                the stars makes me dream."
+              </div>
+              <div
+                className="quote-text blurred-text"
+                style={{ textAlign: 'right' }}
+              >
+                - Vincent Van Gogh
+              </div>
+            </div>
+            <div className="column-container profile">
+              <div className="blurred-text row-container">Nathan Chin</div>
+              <p className="blurred-text row-container">About blurb</p>
+            </div>
+          </div>
+          <div className="column-container">
             <img
               src="../static/profilePic.jpg"
-              alt=""
+              alt="profile goes here"
               className="about-image"
             />
+            <div>Age</div>
+            <div>Hobbies</div>
           </div>
         </div>
-        <div className="blurred-text row-container">Nathan Chin</div>
-        <p className="blurred-text row-container">About blurb</p>
       </div>
     );
   }
