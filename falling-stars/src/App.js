a8ca368572a2441d1190cbc4432024eb0a2b0acd
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-01-31 16:26:07 -0800

    Finalized switch to reach router

--- falling-stars/src/App.js
+++ falling-stars/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar/Navbar';
+import { navItems, siteHead } from './data/nav.json';
+import { Router } from '@reach/router';
+import Contact from './Contact/Contact';
+import About from './About/About';
+import Homepage from './Homepage/Homepage';
+import portfolio from './data/portfolio.json';
+import PortfolioItem from './Portfolio/PortfolioItem';
 import './App.css';
-import SkyOfStars from './SkyofStars/SkyofStars';
 
 class App extends Component {
   render() {
     return (
-      <div className="Sky-Of-Stars">
-        <SkyOfStars />
+      <div className="SkyOfStars">
+        <Navbar navItems={navItems} siteHead={siteHead} />
+        <div className="center-star">
+          <div id="main">
+            <Router>
+              <Homepage default path="/" portfolio={portfolio} />
+              <About path="/about" />
+              <Contact path="contact" />
+              <PortfolioItem path="/portfolio/:item" portfolio={portfolio} />
+            </Router>
+          </div>
+        </div>
       </div>
     );
   }
